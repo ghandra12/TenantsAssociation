48ef6987bf5a030d46ddebebe75c337408d5b368
DESKTOP-N6711EO\radu3 <user24m>
2024-04-11 11:29:15 +0300

    updated adfmin interface

--- tenants-association-fe/src/AdminHomepage/AdminHomepage.js
+++ tenants-association-fe/src/AdminHomepage/AdminHomepage.js
@@ -13,21 +13,20 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 const AdminHomepage=()=>{
 
     
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name) {
+  return { name };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+  
 ];
 const buttons = [
-    <Button key="one">One</Button>,
-    <Button key="two">Two</Button>,
-    <Button key="three">Three</Button>,
+    <Button key="one">Add Invoices</Button>,
+    <Button key="two">Add poll</Button>,
+    <Button key="three">Add services</Button>,
+    <Button key="four">Send messages</Button>,
+    <Button key="five">Create user</Button>,
   ];
 
   return (
@@ -38,11 +37,8 @@ const buttons = [
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableCell sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>Messages from tenants</TableCell>
+           
           </TableRow>
         </TableHead>
         <TableBody>
@@ -70,6 +66,7 @@ const buttons = [
     </ButtonGroup>
   </Grid>
  </Grid>
+ 
    </Box>
   );
 
