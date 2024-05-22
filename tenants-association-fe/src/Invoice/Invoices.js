b84b6f9c1967c09335acb05c294049fe14e03cb5
DESKTOP-B6U7AQJ\Andra <gheorghe.andra.r2x@student.ucv.ro>
2024-05-22 23:23:11 +0300

    added more texts

--- tenants-association-fe/src/Invoice/Invoices.js
+++ tenants-association-fe/src/Invoice/Invoices.js
@@ -16,14 +16,15 @@ import Modal from "@mui/material/Modal";
 import UserContext from "../Services/UserContext";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 450,
-  height: 560,
+  width: 200,
+  height: 200,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -75,89 +76,119 @@ const Invoices = () => {
       sx={{ ml: 20, mr: 20 }}
     >
       <Paper elevation={24}>
-        <TableContainer>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Invoice number</TableCell>
-                <TableCell align="center">Description</TableCell>
-                <TableCell align="center">Release Date</TableCell>
-                <TableCell align="center">Due date</TableCell>
-                <TableCell align="center">Price</TableCell>
-                <TableCell align="center">Remaining</TableCell>
-                <TableCell align="center">Status</TableCell>
-                <TableCell align="center" />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {invoices.map((i) => (
-                <TableRow
-                  key={i.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="center" component="th" scope="row">
-                    {i.invoiceNumber}
-                  </TableCell>
-                  <TableCell align="center">{i.description}</TableCell>
-                  <TableCell align="center">
-                    {i.releaseDate.split("T")[0]}
-                  </TableCell>
-                  <TableCell align="center">{i.dueDate}</TableCell>
-                  <TableCell align="center">{i.sum}</TableCell>
-                  <TableCell align="center">{i.remaining}</TableCell>
-                  <TableCell align="center">
-                    {i.isPaid === true ? "Paid" : "Unpaid"}
-                  </TableCell>
-                  <TableCell align="center">
-                    {i.isPaid === false && (
-                      <>
-                        <Button
-                          variant="outlined"
-                          size="small"
-                          onClick={handleOpenPayment}
-                        >
-                          Pay!
-                        </Button>
-                        <Modal
-                          open={openPayment}
-                          onClose={handleClosePayment}
-                          aria-labelledby="modal-modal-title"
-                          aria-describedby="modal-modal-description"
-                        >
-                          <Box sx={style}>
-                            <FormControl sx={{ maxWidth: 225 }}>
-                              <InputLabel htmlFor="outlined-adornment-amount">
-                                Amount
-                              </InputLabel>
-                              <OutlinedInput
-                                id="outlined-adornment-amount"
-                                startAdornment={
-                                  <InputAdornment position="start">
-                                    $
-                                  </InputAdornment>
-                                }
-                                label="Amount"
-                                value={sum}
-                                onChange={onChangeSumHandler}
-                              />
-                            </FormControl>
-                            <Button
-                              color="secondary"
-                              onClick={() => onAddPayment(i.id)}
-                              variant="contained"
-                            >
-                              Add payment
-                            </Button>
-                          </Box>
-                        </Modal>
-                      </>
-                    )}
-                  </TableCell>
+        {invoices.length === 0 && (
+          <Typography
+            variant="h5"
+            color="secondary"
+            paddingBlock={6}
+            paddingInline={6}
+          >
+            Currently you don't have any invoices!
+          </Typography>
+        )}
+        {invoices.length !== 0 && (
+          <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Invoice number</TableCell>
+                  <TableCell align="center">Description</TableCell>
+                  <TableCell align="center">Release Date</TableCell>
+                  <TableCell align="center">Due date</TableCell>
+                  <TableCell align="center">Price</TableCell>
+                  <TableCell align="center">Remaining</TableCell>
+                  <TableCell align="center">Status</TableCell>
+                  <TableCell align="center" />
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {invoices.map((i) => (
+                  <TableRow
+                    key={i.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="center" component="th" scope="row">
+                      {i.invoiceNumber}
+                    </TableCell>
+                    <TableCell align="center">{i.description}</TableCell>
+                    <TableCell align="center">
+                      {i.releaseDate.split("T")[0]}
+                    </TableCell>
+                    <TableCell align="center">
+                      {" "}
+                      {i.dueDate.split("T")[0]}
+                    </TableCell>
+                    <TableCell align="center">{i.sum}</TableCell>
+                    <TableCell align="center">{i.remaining}</TableCell>
+                    <TableCell align="center">
+                      {i.isPaid === true ? "Paid" : "Unpaid"}
+                    </TableCell>
+                    <TableCell align="center">
+                      {i.isPaid === false && (
+                        <>
+                          <Button
+                            variant="contained"
+                            size="small"
+                            onClick={handleOpenPayment}
+                            color="primary"
+                          >
+                            Pay!
+                          </Button>
+                          <Modal
+                            open={openPayment}
+                            onClose={handleClosePayment}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                          >
+                            <Box sx={style}>
+                              <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                  <Typography variant="h7" color="secondary">
+                                    How much do you want to pay?
+                                  </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                  <FormControl sx={{ maxWidth: 225 }}>
+                                    <InputLabel
+                                      mb={10}
+                                      htmlFor="outlined-adornment-amount"
+                                    >
+                                      Amount
+                                    </InputLabel>
+                                    <OutlinedInput
+                                      id="outlined-adornment-amount"
+                                      startAdornment={
+                                        <InputAdornment position="start">
+                                          $
+                                        </InputAdornment>
+                                      }
+                                      label="Amount"
+                                      value={sum}
+                                      onChange={onChangeSumHandler}
+                                    />
+                                  </FormControl>
+                                </Grid>
+                                <Grid item xs={12}>
+                                  <Button
+                                    color="secondary"
+                                    onClick={() => onAddPayment(i.id)}
+                                    variant="contained"
+                                  >
+                                    Add payment
+                                  </Button>
+                                </Grid>
+                              </Grid>
+                            </Box>
+                          </Modal>
+                        </>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
       </Paper>
     </Box>
   );
